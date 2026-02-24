a77a7e090910c52a94a988a3f6a79e20d641fe7c
Madchili <jonas_nl@live.se>
2026-02-24 13:40:25 +0100

    Added responsive header and simulated login. Also added fonts to global styling.

--- public/js/header.js
+++ public/js/header.js
@@ -1,21 +1,51 @@
-const hamburgerButton = document.querySelector('.hamburger-button');
+const hamburgerButton = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
+// const listOption = document.querySelector('.list-option');
 const backdrop = document.querySelector('.backdrop');
 const buttonbar1 = document.querySelector('.bar1');
 const buttonbar2 = document.querySelector('.bar2');
 const buttonbar3 = document.querySelector('.bar3');
 const body = document.querySelector('body');
+const loggedInMenu = document.querySelector('.loggedin-menu');
+const loggedOutMenu = document.querySelector('.loggedout-menu');
+
+// Test login header
+
+const state = {
+  isLoggedIn: false,
+};
+
+const testLoginButton = document.querySelector('.test-login');
+testLoginButton.addEventListener('click', () => {
+  testLoginButton.classList.toggle('active');
+  state.isLoggedIn = !state.isLoggedIn;
+  console.log('User logged in:', state.isLoggedIn);
+  updateMenu();
+});
+
+function updateMenu() {
+  if (state.isLoggedIn) {
+    loggedInMenu.classList.add('loggedIn');
+    loggedOutMenu.classList.add('loggedIn');
+  } else {
+    loggedInMenu.classList.remove('loggedIn');
+    loggedOutMenu.classList.remove('loggedIn');
+  }
+}
+// Slut på Test login Header
 
 if (hamburgerButton && navMenu && backdrop) {
   hamburgerButton.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     backdrop.classList.toggle('active');
+    // listOption.classList.toggle('active');
     body.classList.toggle('no-scroll');
     updateHamburgerButton();
   });
   backdrop.addEventListener('click', () => {
     navMenu.classList.remove('active');
     backdrop.classList.remove('active');
+    // listOption.classList.remove('active');
     body.classList.remove('no-scroll');
     updateHamburgerButton();
   });
