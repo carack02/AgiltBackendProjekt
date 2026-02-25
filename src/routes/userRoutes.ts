756f141b931a055dd1924916ea20284207040ada
Camilla <camilla.johansson1986@gmail.com>
2026-02-25 21:00:41 +0100

    Added updateUser

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -2,9 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
-import { getUsers, createUser } from '../controllers/userController.ts';
+import {
+  getUsers,
+  createUser,
+  updateUser,
+} from '../controllers/userController.ts';
 
 router.get('/users', getUsers);
 router.post('/user', createUser);
+router.post('/user', updateUser);
 
 export default router;
