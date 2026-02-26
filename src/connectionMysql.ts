7750575cee464b1891422cf646fb151782c5a504
Madchili <jonas_nl@live.se>
2026-02-26 08:34:59 +0100

    Connection file

---
+++ src/connectionMysql.ts
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+
+const mysqlpool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASS,
+  database: process.env.MYSQL_DATABASE,
+});
+
+export default mysqlpool;
