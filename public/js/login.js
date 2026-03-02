136b05882084d3bdd28f1e28bdda4a5e847137e6
Camilla <camilla.johansson1986@gmail.com>
2026-03-02 23:40:46 +0100

    Added functionality to login user

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+const inputUsername = document.querySelector('#username');
+const inputPassword = document.querySelector('#password');
+// const errorMessage = document.querySelector('#error-message');
+
+async function loginUser(event) {
+  event.preventDefault();
+  let username = inputUsername.value;
+  console.log('username', username);
+  let userPassword = inputPassword.value;
+  console.log('password', userPassword);
+
+  try {
+    const res = await fetch('http://localhost:3000/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ username, userPassword }),
+    });
+    const data = await res.json();
+    console.log('data', data);
+    if (!res.ok) {
+      console.log('data.error', data.error);
+      return;
+    }
+    localStorage.setItem('username', JSON.stringify(username));
+    window.location.href = 'index.html';
+  } catch (err) {
+    console.error('Error logging in user', err);
+  }
+}
+form.addEventListener('submit', loginUser);
