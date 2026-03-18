c0b0460efc2a1516feb679db80e7d26406e8f90b
Madchili <jonas_nl@live.se>
2026-03-18 13:05:17 +0100

    added styling and functionality for solving quiz

--- public/js/quizResult.js
+++ public/js/quizResult.js
@@ -4,33 +4,53 @@ const parmans = new URLSearchParams(document.location.search);
 const quizResult = document.querySelector('.quizResult');
 const quiz = parmans.get('id');
 console.log(quiz);
-
+const scoreContainer = document.querySelector('scoreContainer');
 const selectedQuiz = await getQuizcards({ collectionId: quiz });
+const score = document.querySelector('score');
+let correct;
+let wrong;
 console.log(selectedQuiz);
 listResults();
 
 function listResults() {
   for (const quizQuestion of selectedQuiz) {
     const questionContainer = document.createElement('div');
-    questionContainer.classList.add('question');
+    questionContainer.classList.add('questionBox');
+    questionContainer.classList.add('card');
     const question = document.createElement('div');
     question.classList.add('question');
+    question.classList.add('card');
     const answer = document.createElement('div');
     const correctElement = document.createElement('div');
-
+    correctElement.classList.add('correctElement');
+    const correctAnswerText = document.createElement('div');
+    correctAnswerText.innerHTML = `Rätt svar: ${quizQuestion.quizCorrectAnswer}`;
+    correctAnswerText.classList.add('correctAnswerText');
+    correctAnswerText.classList.add('hidden');
+    const revealAnswerButton = document.createElement('button');
+    revealAnswerButton.classList.add('revealAnswerButton');
+    revealAnswerButton.innerHTML = 'Visa rätt svar >';
+    revealAnswerButton.addEventListener('click', () => {
+      revealAnswerButton.classList.add('hidden');
+      correctAnswerText.classList.remove('hidden');
+    });
     const storedAnswer = localStorage.getItem(
       `collection${quiz}question${quizQuestion.quizId}`,
     );
     question.innerHTML = quizQuestion.quizQuestion;
 
-    answer.innerHTML = storedAnswer;
+    answer.innerHTML = `Du svarade: ${storedAnswer}`;
     answer.classList.add('answer');
     if (quizQuestion.quizCorrectAnswer === storedAnswer) {
       answer.classList.add('correct');
+      correct++;
     } else {
       answer.classList.add('wrong');
-      correctElement.innerHTML = `Rätt svar var: ${quizQuestion.quizCorrectAnswer}`;
-      correctElement.classList.add('hidden');
+
+      correctElement.appendChild(correctAnswerText);
+      correctElement.appendChild(revealAnswerButton);
+      // correctElement.classList.add('hidden');
+      wrong++;
     }
 
     questionContainer.appendChild(question);
